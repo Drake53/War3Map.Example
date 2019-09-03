dc5723c89cf8ffc1c6b9355359ad0932d19fdb8a
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-09-03 22:18:26 +0200

    Implement launcher to run War3Net.Build and Warcraft III, and include example C# code to transpile to war3map.lua.

---
+++ War3Map.Example.Launcher/Program.cs
@@ -0,0 +1,53 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Drake53">
+// Copyright (c) 2019 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using System.Diagnostics;
+using System.IO;
+
+using War3Net.Build;
+using War3Net.Build.Script;
+
+namespace War3Map.Example.Launcher
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            // Configuration
+            var scriptBuilderOptions = new ScriptBuilderOptions();
+            // TODO: set map name and description (note: game reads these from the .w3i file, so changing these doesn't really do anything)
+            scriptBuilderOptions.MapName = "Just Another Warcraft III Map";
+            scriptBuilderOptions.MapDescription = "Source code generated with CSharp.lua\r\nMap generated with War3Net.Build";
+            // TODO: set player slots
+            scriptBuilderOptions.PlayerSlots.Add(new PlayerSlot(0f, 0f, 0, true));
+            scriptBuilderOptions.PlayerSlots.Add(new PlayerSlot(0f, 0f, 1, false));
+
+            var scriptCompilerOptions = new ScriptCompilerOptions();
+            scriptCompilerOptions.BuilderOptions = scriptBuilderOptions;
+            // TODO: rename project(s)?
+            scriptCompilerOptions.SourceDirectory = @"..\..\..\..\War3Map.Example.Source";
+            // TODO: set output directory
+            scriptCompilerOptions.OutputDirectory = @"";
+
+            // TODO: set path to Warcraft III.exe
+            var wc3executablePath = @"";
+            // TODO: optionally customize commandline args
+            var wc3executableArguments = "-nowfpause -graphicsapi Direct3D9 ";
+
+            // Build and launch
+            var mapName = "TestMap.w3x";
+            var mapBuilder = new MapBuilder(mapName);
+
+            // TODO: set path(s) to load assets (this can be a .w3m/.w3x file, or a directory)
+            if (mapBuilder.Build(scriptCompilerOptions, @"", @""))
+            {
+                var mapPath = Path.Combine(scriptCompilerOptions.OutputDirectory, mapName);
+                Process.Start(wc3executablePath, $"{wc3executableArguments} -loadfile \"{mapPath}\"");
+            }
+        }
+    }
+}
---
+++ War3Map.Example.Source/Program.cs
@@ -0,0 +1,25 @@
+﻿// ------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Drake53">
+// Copyright (c) 2019 Drake53. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+// ------------------------------------------------------------------------------
+
+using static War3Api.Common;
+
+namespace War3Map.Example.Source
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            TimerStart(CreateTimer(), 1f, false, HelloWorld);
+        }
+
+        private static void HelloWorld()
+        {
+            DestroyTimer(GetExpiredTimer());
+            BlzDisplayChatMessage(Player(0), 0, "Hello World!");
+        }
+    }
+}
