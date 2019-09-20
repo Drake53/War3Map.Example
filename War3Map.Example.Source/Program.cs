84d1cd0c8056e87c36ef2650ad28d195bbf34dab
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-09-20 12:33:14 +0200

    Update example to showcase attaching data to timers without a hashtable.

--- War3Map.Example.Source/Program.cs
+++ War3Map.Example.Source/Program.cs
@@ -5,6 +5,8 @@
 // </copyright>
 // ------------------------------------------------------------------------------
 
+using System;
+
 using static War3Api.Common;
 
 namespace War3Map.Example.Source
@@ -13,13 +15,67 @@ namespace War3Map.Example.Source
     {
         private static void Main()
         {
-            TimerStart(CreateTimer(), 1f, false, HelloWorld);
+            using (var msg = new Message(Player(0), Message.RECIPIENT_ALL, "Hello World!"))
+            {
+                msg.Display(1f);
+            }
+        }
+    }
+
+    public sealed class Message : IDisposable
+    {
+        public const int RECIPIENT_ALL = 0;
+        public const int RECIPIENT_ALLIES = 1;
+        public const int RECIPIENT_OBSERVERS = 2;
+        public const int RECIPIENT_PRIVATE = 3;
+
+        private readonly timer _timer;
+        private readonly player _player;
+        private readonly int _recipient;
+        private readonly string _message;
+
+        private bool _timerRunning;
+        private bool _disposing;
+
+        public Message(player player, int recipient, string message)
+        {
+            _timer = CreateTimer();
+            _player = player;
+            _recipient = recipient;
+            _message = message;
+
+            _timerRunning = false;
+            _disposing = false;
         }
 
-        private static void HelloWorld()
+        public void Display(float timeout)
         {
-            DestroyTimer(GetExpiredTimer());
-            BlzDisplayChatMessage(Player(0), 0, "Hello World!");
+            _timerRunning = true;
+            TimerStart(_timer, timeout, false, OnDisplay);
+        }
+
+        public void Dispose()
+        {
+            if (!_timerRunning)
+            {
+                // PauseTimer(_timer);
+                DestroyTimer(_timer);
+            }
+            else
+            {
+                _disposing = true;
+            }
+        }
+
+        private void OnDisplay()
+        {
+            _timerRunning = false;
+            BlzDisplayChatMessage(_player, _recipient, _message);
+
+            if (_disposing)
+            {
+                Dispose();
+            }
         }
     }
 }
