b163a1a1b10d13279a7ad5c9d84624bbd713b60c
Drake53 <49623303+Drake53@users.noreply.github.com>
2019-10-08 21:28:16 +0200

    Update example with project and package references.

---
+++ War3Map.Example.Source.Dependency/Message.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+using War3Lib.Extensions.Common;
+
+using static War3Api.Common;
+
+namespace War3Map.Example.Source.Dependency
+{
+    public sealed class Message : IDisposable
+    {
+        public const int RECIPIENT_ALL = 0;
+        public const int RECIPIENT_ALLIES = 1;
+        public const int RECIPIENT_OBSERVERS = 2;
+        public const int RECIPIENT_PRIVATE = 3;
+
+        private readonly timer _timer;
+        private readonly player _player;
+        private readonly int _recipient;
+        private readonly string _message;
+
+        private bool _timerRunning;
+        private bool _disposing;
+
+        public Message(player player, int recipient, string message)
+        {
+            _timer = CreateTimer();
+            _player = player;
+            _recipient = recipient;
+            _message = message;
+
+            _timerRunning = false;
+            _disposing = false;
+        }
+
+        public void Display(float timeout)
+        {
+            _timerRunning = true;
+            _timer.Start(timeout, false, OnDisplay);
+        }
+
+        public void Dispose()
+        {
+            if (!_timerRunning)
+            {
+                // PauseTimer(_timer);
+                DestroyTimer(_timer);
+            }
+            else
+            {
+                _disposing = true;
+            }
+        }
+
+        private void OnDisplay()
+        {
+            _timerRunning = false;
+            BlzDisplayChatMessage(_player, _recipient, _message);
+
+            if (_disposing)
+            {
+                Dispose();
+            }
+        }
+    }
+}
--- War3Map.Example.Source/Program.cs
+++ War3Map.Example.Source/Program.cs
@@ -5,7 +5,7 @@
 // </copyright>
 // ------------------------------------------------------------------------------
 
-using System;
+using War3Map.Example.Source.Dependency;
 
 using static War3Api.Common;
 
@@ -18,63 +18,6 @@ namespace War3Map.Example.Source
             using (var msg = new Message(Player(0), Message.RECIPIENT_ALL, "Hello World!"))
             {
                 msg.Display(1f);
-            }
-        }
-    }
-
-    public sealed class Message : IDisposable
-    {
-        public const int RECIPIENT_ALL = 0;
-        public const int RECIPIENT_ALLIES = 1;
-        public const int RECIPIENT_OBSERVERS = 2;
-        public const int RECIPIENT_PRIVATE = 3;
-
-        private readonly timer _timer;
-        private readonly player _player;
-        private readonly int _recipient;
-        private readonly string _message;
-
-        private bool _timerRunning;
-        private bool _disposing;
-
-        public Message(player player, int recipient, string message)
-        {
-            _timer = CreateTimer();
-            _player = player;
-            _recipient = recipient;
-            _message = message;
-
-            _timerRunning = false;
-            _disposing = false;
-        }
-
-        public void Display(float timeout)
-        {
-            _timerRunning = true;
-            TimerStart(_timer, timeout, false, OnDisplay);
-        }
-
-        public void Dispose()
-        {
-            if (!_timerRunning)
-            {
-                // PauseTimer(_timer);
-                DestroyTimer(_timer);
-            }
-            else
-            {
-                _disposing = true;
-            }
-        }
-
-        private void OnDisplay()
-        {
-            _timerRunning = false;
-            BlzDisplayChatMessage(_player, _recipient, _message);
-
-            if (_disposing)
-            {
-                Dispose();
             }
         }
     }
